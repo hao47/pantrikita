852e0bbc8950dca873a7d2aa5e87d6718cd5d1b6
SyahranFD <fadhilrafa1@gmail.com>
2025-07-24 18:17:52 +0700

    feat: [SF] add folder bloc in pantry and widget dropdown filter pantry

--- lib/feature/pantry/presentation/pages/pantry_page.dart
+++ lib/feature/pantry/presentation/pages/pantry_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pantrikita/core/theme/color_value.dart';
 import 'package:pantrikita/core/theme/text_style.dart';
+import 'package:pantrikita/feature/pantry/data/dropdown_data/category_dropdown_data.dart';
+import 'package:pantrikita/feature/pantry/data/dropdown_data/sort_dropdown_data.dart';
+import 'package:pantrikita/feature/pantry/data/dropdown_data/status_dropdown_data.dart';
+import 'package:pantrikita/feature/pantry/presentation/bloc/pantry_bloc.dart';
 import 'package:pantrikita/feature/pantry/presentation/widgets/card_summary_pantry.dart';
+import 'package:pantrikita/feature/pantry/presentation/widgets/dropdown_filter_pantry.dart';
 
 class PantryPage extends StatelessWidget {
   const PantryPage({super.key});
@@ -36,6 +42,42 @@ class PantryPage extends StatelessWidget {
                   ),
 
                   SizedBox(height: 20),
+
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        BlocBuilder<PantryBloc, PantryState>(builder: (context, state) {
+                          return DropdownFilterPantry(
+                            items: categoryDropdownData,
+                            selectedValue: state.filterCategory,
+                            onChanged: (value) {
+                              context.read<PantryBloc>().add(GetFilterCategoryEvent(filterCategory: value));
+                            },
+                          );
+                        }),
+
+                        BlocBuilder<PantryBloc, PantryState>(builder: (context, state) {
+                          return DropdownFilterPantry(
+                            items: statusDropdownData,
+                            selectedValue: state.filterStatus,
+                            onChanged: (value) {
+                              context.read<PantryBloc>().add(GetFilterStatusEvent(filterStatus: value));
+                            },
+                          );
+                        }),
+
+                        BlocBuilder<PantryBloc, PantryState>(builder: (context, state) {
+                          return DropdownFilterPantry(
+                            items: sortDropdownData,
+                            selectedValue: state.filterSort,
+                            onChanged: (value) {
+                              context.read<PantryBloc>().add(GetFilterSortEvent(filterSort: value));
+                            },
+                          );
+                        }),
+                      ]
+                  )
                 ],
               ),
             ),
